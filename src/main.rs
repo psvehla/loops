42d86f3055464999480dba22159d3847f07b2ae6
psvehla <psvehla@hotmail.com>
2023-11-21 21:28:45 +1100

    next bit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    loop {
-        println!("again!");
-    }
+    // loop {
+    //     println!("again!");
+    // }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
